f05a94eb9a4c9fd9f04685ea1f2fd4587e32fa9c
jdaal19 <jantimanlagos@gmail.com>
2022-08-14 20:48:13 -0400

    Create 2.calculadora.R

---
+++ scripts2/2.calculadora.R
@@ -0,0 +1,196 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3+5
+
+2%%3
+
+2^3*5
+
+2^-4
+
+725%/%7
+
+725%%7
+# D = d * q + r
+# r = D - d * q
+# q = D %/% d
+# r = D %% d
+
+725- 103*7
+
+pi
+
+2*pi
+
+3^pi
+
+pi^2
+
+Inf
+
+-Inf
+
+NA
+
+NaN
+
+5/0
+
+0/0
+
+2^50 #1.1259e+15 = 1125900000000000
+
+2^(-15) #3.051758e-05 = 0.00003051758
+
+c(2^30, 2^(-15), 1, 2, 3/2)
+
+
+sqrt(5)
+
+sqrt(1)
+
+log(pi)
+
+log(32, 2)
+
+abs(-pi)
+
+factorial(7)
+
+factorial(4)
+
+factorial(0)
+
+log(4^6, 4)
+
+6^log(4, 6)
+
+choose(5, 3)
+
+choose(3, 5)
+
+choose(3,1)
+
+sin(60*pi/180)
+
+cos(60*pi/180)
+
+sinpi(1/2) # = sin(pi/2)
+
+tan(pi) # -1.224647e-16 ~ 0
+
+tan(pi/2) # 1.633124e+16 ~ inf
+
+asin(0.8660254) # arc sin en radianes
+
+asin(0.8660254) *180/pi #arc sin en grados
+
+print(sqrt(2),10)
+
+round(sqrt(2),3)
+floor(sqrt(2))
+ceiling(sqrt(2))
+trunc(sqrt(2))
+
+sqrt(2)^2
+sqrt(2)^2-2
+
+2^50
+print(2^50,15)
+print(2^50,2)
+
+round(sqrt(2),4)^2
+round(1.15,1)
+round(1.25,1)
+round(1.35,1)
+round(sqrt(2),0)
+round(sqrt(2))
+
+
+
+round(digits = 5, sqrt(2))
+round(5, sqrt(2))
+
+
+floor(-3.45)
+ceiling(-3.45)
+trunc(-3.45)
+
+
+
+(pi^2)/2
+x = (pi^2)/2
+x
+
+
+y <- cos(pi/4)
+y
+
+
+sin(pi/4) + cos(pi/4) -> z
+z
+
+
+edad <- 30
+
+nombre = "Juan Antiman"
+
+HOLA = 1
+hola = 5
+
+
+pi.4 = 4*pi
+pi.4
+
+
+x = 2
+x = x^2
+
+
+x = sqrt(x)
+
+## Funcion f(x) = x^3 - (3^x) * sen(x)
+
+f = function(x) {
+  x^3 - (3^x) *sin(x)
+}
+
+f(4) 
+f(pi/2)
+
+suma1 <-  function(t){
+  t + 1
+}
+suma1(6)
+suma1(5)
+
+product <- function(x, y){
+  x*y
+}
+product(5,7)
+
+g <- function(x,y,z){
+  exp(x^2 + y^2)*sin(z)
+}
+
+g(1, 2, 3)
+g(1, -1, pi)
+
+suma5 <- function(numero){
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  suma1(numero)
+}
+
+suma5(3)
+ls()
+rm(product)
+ls()
+rm(list = ls())
