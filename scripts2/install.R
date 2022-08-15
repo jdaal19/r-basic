a2f43987827d340ed57d6cd142d1b3c5f7659cd5
jdaal19 <jantimanlagos@gmail.com>
2022-08-14 20:48:20 -0400

    Create install.R

---
+++ scripts2/install.R
@@ -0,0 +1,15 @@
+install.packages("tidyverse",dep = TRUE) 
+
+# instala paquetes por consola y todos
+#sus paquetes asociados para su funcionamiento
+
+library(tidyverse)
+
+#activa el paquete y todos los paquetes asociados para el funcionamiento.
+
+install.packages("magic", dep = TRUE)
+library(magic)
+
+magic(6)
+
+installed.packages()
